be7eb15e0c7851546e8a977bf770071938724bdb
Edgar Thier <edt@lavabit.com>
2012-11-14 18:59:43 +0100

    Multiple updates
        Functions to rename buffers
        colortheme now always active
        debug on error always active
        changed path variable
        several debugs

--- custom.el
+++ custom.el
@@ -3,3 +3,15 @@
 ;;------------------------------------------------------------------------------
 
 ;; currently empty
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(exec-path (quote ("/usr/local/bin" "/usr/bin" "/bin" "/usr/local/games" "/usr/games" "/usr/lib/emacs/24.2/x86_64-linux-gnu" "/home/edt/bin"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -29,6 +29,11 @@
 ;;------------------------------------------------------------------------------
 
 
+;; http://www.chemie.fu-berlin.de/chemnet/use/info/elisp/elisp_16.html
+(add-hook 'after-init-hook
+          '(lambda () (setq debug-on-error t)))
+
+
 (add-to-list 'load-path "/usr/share/emacs/site-lisp")
 (add-to-list 'load-path "/home/edt/.emacs.d")
 (add-to-list 'load-path "/home/edt/.emacs.d/lisp")
@@ -108,8 +113,8 @@
 
 ;; prefer vertical window split over horizontal
 ;; http://stackoverflow.com/questions/2081577/setting-emacs-split-to-horizontal
-(setq split-height-threshold nil)
-(setq split-width-threshold 0)
+;;(setq split-height-threshold nil)
+;;(setq split-width-threshold 0)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                    GENERAL
@@ -124,6 +129,9 @@
 ;(setq uniquify-buffer-name-style 'reverse)
 
 ;;;;;;;;;;;;;;;;;;; Appearance ;;;;;;;;;;;;;;;;;;;
+
+(load "my-color-theme.el")
+(my-color-theme )
 
 
 ;;;;;;;;;;;;;;;;;;;;== Menu ==;;;;;;;;;;;;;;;;;;;;
@@ -221,7 +229,7 @@
   (make-directory "~/.emacs.d/tmp/" 'parent))
 
 ;; set directory for backups
-(setq backup-directory-alist (".", "~/.emacs.d/tmp"))
+;;(setq backup-directory-alist "~/.emacs.d/tmp")
 
 ;; make backups by copying 
 ;; this prevents emacs from changing the creation time of the original file
@@ -229,6 +237,9 @@
 
 ;; set directory for auto saved files
 (setq auto-save-directory "~/.emacs.d/tmp/")
+
+(setq backup-directory-alist
+      `((".*" . ,"~/.emacs.d/tmp")))
 
 ;; always have an empty line at end of file
 (setq require-final-newline t)
@@ -242,6 +253,39 @@
 ;(setq case-fold-search t)
 
 
+;; https://sites.google.com/site/steveyegge2/my-dot-emacs-file
+;;
+;; Never understood why Emacs doesn't have this function.
+;;
+(defun rename-file-and-buffer (new-name)
+ "Renames both current buffer and file it's visiting to NEW-NAME." (interactive "sNew name: ")
+ (let ((name (buffer-name))
+	(filename (buffer-file-name)))
+ (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+ (if (get-buffer new-name)
+	 (message "A buffer named '%s' already exists!" new-name)
+	(progn 	 (rename-file name new-name 1)
+                 (rename-buffer new-name)
+                 (set-visited-file-name new-name)
+                 (set-buffer-modified-p nil)))))) ;;
+;; Never understood why Emacs doesn't have this function, either.
+;;
+(defun move-buffer-file (dir)
+ "Moves both current buffer and file it's visiting to DIR." (interactive "DNew directory: ")
+ (let* ((name (buffer-name))
+	 (filename (buffer-file-name))
+	 (dir
+	 (if (string-match dir "\\(?:/\\|\\\\)$")
+	 (substring dir 0 -1) dir))
+	 (newname (concat dir "/" name)))
+
+ (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+ (progn 	(copy-file filename newname 1)
+                (delete-file filename)
+                (set-visited-file-name newname)
+                (set-buffer-modified-p nil) 	t))))
 
 
 

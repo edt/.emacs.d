8f74c6254ca37b78970063147443f6e0a67777bb
Edgar Thier <edgarthier@gmail.com>
2013-04-09 15:44:01 +0200

    Added configuration for highlight-symbol

--- modes.el
+++ modes.el
@@ -16,3 +16,10 @@
 
 (require 'buffer-move)
 
+(require 'highlight-symbol)
+(global-set-key (kbd "C-<f3>") 'highlight-symbol-at-point)
+(global-set-key (kbd "<f3>") 'highlight-symbol-next)
+(global-set-key (kbd "S-<f3>") 'highlight-symbol-prev)
+(global-set-key (kbd "M-<f3>") 'highlight-symbol-remove-all)
+
+

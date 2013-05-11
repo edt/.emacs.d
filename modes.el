63826df4d86f7dd8912dfcfe7097a372eb95a0c6
Edgar Thier <edgarthier@gmail.com>
2013-05-11 20:57:56 +0200

    Added saving of variables by desktop

--- modes.el
+++ modes.el
@@ -11,6 +11,22 @@
 (setq ido-max-directory-size 1000000)
 (setq ido-default-buffer-method 'selected-window)
 
+;; save a bunch of variables to the desktop file
+;; for lists specify the len of the maximal saved data also
+(setq desktop-globals-to-save
+      (append '((extended-command-history . 30)
+                (file-name-history        . 100)
+                (grep-history             . 30)
+                (compile-history          . 30)
+                (minibuffer-history       . 50)
+                (query-replace-history    . 60)
+                (read-expression-history  . 60)
+                (regexp-history           . 60)
+                (regexp-search-ring       . 20)
+                (search-ring              . 20)
+                (shell-command-history    . 50)
+                tags-file-name
+                register-alist)))
 
 (require 'yasnippet)
 (yas/global-mode 1)

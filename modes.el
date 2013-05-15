c40405bad84edb026d5e942fbade143804268706
Edgar Thier <edgarthier@gmail.com>
2013-05-15 17:44:56 +0200

    Added yafolding and corresponding default config

--- modes.el
+++ modes.el
@@ -33,7 +33,18 @@
 (yas/load-directory "~/.emacs.d/snippets")
 (setq yas/indent-line nil) ;disable yasnippet auto-indent
 
-(require 'buffer-move)
+(require 'yafolding)
+(define-key global-map (kbd "C-'") 'yafolding)
+;;(define-key global-map (kbd "C-c C-f") 'yafolding-toggle-all)
+(define-key global-map (kbd "C-c C-f") 'yafolding-toggle-all-by-current-level)
+;;(add-hook 'indent-buffer-before-hook
+;;    (lambda ()
+;;      (yafolding-temp-toggle nil)))
+;;(add-hook 'indent-buffer-after-hook
+;;    (lambda ()
+;;      (yafolding-temp-toggle t)))
+
+;(require 'buffer-move)
 
 (require 'window-number)
 (window-number-mode t)

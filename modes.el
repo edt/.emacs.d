011ba3b143b9ac789d74c5f67cbe6833e4fc8a91
Edgar Thier <edgarthier@gmail.com>
2013-05-22 23:56:10 +0200

    Added hippie-expand

--- modes.el
+++ modes.el
@@ -74,3 +74,9 @@
 
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-line try-complete-lisp-symbol-partially try-complete-lisp-symbol))
+
+

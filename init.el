1849f736324a6b9837f8857f9c391093025f69a4
Edgar Thier <edgarthier@gmail.com>
2013-05-22 21:18:50 +0200

    extracted el-get config into own file

--- init.el
+++ init.el
@@ -3,37 +3,6 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 
-;;------------------------------------------------------------------------------
-;; el-get
-;;------------------------------------------------------------------------------
-
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-;; Wenn el-get nicht geladen werden kann, lädt er die aktuelle (development)
-;; Version herunter und installiert es.
-(unless (require 'el-get nil t)
-  (with-current-buffer
-      (url-retrieve
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-       (lambda (s)
-         (let (el-get-master-branch)
-           (goto-char (point-max))
-           (eval-print-last-sexp))))))
-
-;; Hier die packages hinzufügen
-;; Namen kann man im el-get repository nachschauen (recipes ordner)
-;; Alternativ auch halt eigene recipes schreiben
-(setq my-packages
-      '(el-get
-        color-theme
-        smex
-        highlight-symbol
-        multiple-cursors
-        ace-jump-mode
-        yasnippet))
-
-;; Initialisiere alles und so...
-(el-get 'sync my-packages)
 
 ;;------------------------------------------------------------------------------
 
@@ -53,11 +22,14 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
+;(require 'init-packages)
+(load "init-packages")
 ;; Load the following configuration files
 (load "user_profile.el")
 (load "functions.el")
 (load "modes.el")
 (load "programming.el")
+(load "compilation.el")
 (load "keybindings.el")
 
 ;; directories for .emacs.desktop and .emacs.desktop.lock files

592ef2b9a04eb983312525d24658f0c22d9b4a67
Edgar Thier <edgarthier@gmail.com>
2013-05-25 13:48:33 +0200

    Further cleanup concerning session management
        Moved desktop variables from modes.el
        Changed desktop folder to tmp

--- setup-session.el
+++ setup-session.el
@@ -1,8 +1,27 @@
 
 
 ;; directories for .emacs.desktop and .emacs.desktop.lock files
-(setq desktop-path '("~/.emacs.d/"))
-(setq desktop-dirname "~/.emacs.d/")
+(setq desktop-path '("~/.emacs.d/tmp"))
+(setq desktop-dirname "~/.emacs.d/tmp")
+
+
+;; save a bunch of variables to the desktop file
+;; for lists specify the len of the maximal saved data also
+(setq desktop-globals-to-save
+      (append '((extended-command-history . 30)
+                (file-name-history        . 100)
+                (grep-history             . 30)
+                (compile-history          . 30)
+                (minibuffer-history       . 50)
+                (query-replace-history    . 60)
+                (read-expression-history  . 60)
+                (regexp-history           . 60)
+                (regexp-search-ring       . 20)
+                (search-ring              . 20)
+                (shell-command-history    . 50)
+                tags-file-name
+                register-alist)))
+
 
 ;; Save session before quitting
 (desktop-save-mode t)

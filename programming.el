74155308d26e5fe766180fa3607509a80ad69212
Edgar Thier <edgarthier@gmail.com>
2013-05-25 13:58:38 +0200

    Indentation cleanup

--- programming.el
+++ programming.el
@@ -3,45 +3,40 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun my-c-mode-common-hook ()
- ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
- (c-set-offset 'substatement-open 0)
- ;; other customizations can go here
+  ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
+  (c-set-offset 'substatement-open 0)
+  ;; other customizations can go here
 
- (setq c++-tab-always-indent t)
- (setq c-basic-offset 4)                  ;; Default is 2
- (setq c-indent-level 4)                  ;; Default is 2
+  (setq c++-tab-always-indent t)
+  (setq c-basic-offset 4) ;; Default is 2
+  (setq c-indent-level 4) ;; Default is 2
 
- (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
- (setq tab-width 4)
- (setq indent-tabs-mode nil)  ; use spaces only if nil
- )
+  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+  (setq tab-width 4)
+  (setq indent-tabs-mode nil))  ; use spaces only if nil
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
 
 ;;indent-style
- (setq c-default-style "bsd"
-       c-basis-offset 4
-       c-indent-level 4
-       tab-width 4)
+(setq c-default-style "bsd"
+      c-basis-offset 4
+      c-indent-level 4
+      tab-width 4)
 
 (setq c-mode-hook
-    (function (lambda ()
-                (setq indent-tabs-mode nil)
-                (setq c-indent-level 4))))
+      (function (lambda ()
+                  (setq indent-tabs-mode nil)
+                  (setq c-indent-level 4))))
 (setq objc-mode-hook
-    (function (lambda ()
-                (setq indent-tabs-mode nil)
-                (setq c-indent-level 4))))
+      (function (lambda ()
+                  (setq indent-tabs-mode nil)
+                  (setq c-indent-level 4))))
 (setq c++-mode-hook
-    (function (lambda ()
-                (setq indent-tabs-mode nil)
-                (setq c-indent-level 4))))
+      (function (lambda ()
+                  (setq indent-tabs-mode nil)
+                  (setq c-indent-level 4))))
 
 ;; Perl section
 
-    ;;; cperl-mode is preferred to perl-mode                                        
-    (defalias 'perl-mode 'cperl-mode)
-
-
-
-
+;; cperl-mode is preferred to perl-mode
+(defalias 'perl-mode 'cperl-mode)

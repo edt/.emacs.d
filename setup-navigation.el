2b687228217ea4b6164caccedb2651acf270a39a
Edgar Thier <edgarthier@gmail.com>
2013-07-15 08:03:29 +0200

    Introduced file for navigation functionality

---
+++ setup-navigation.el
@@ -0,0 +1,25 @@
+;; Move more quickly
+;; taken from whattheemacsd.com
+(global-set-key (kbd "C-S-n")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (next-line 5))))
+
+(global-set-key (kbd "C-S-p")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (previous-line 5))))
+
+(global-set-key (kbd "C-S-f")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (forward-char 5))))
+
+(global-set-key (kbd "C-S-b")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (backward-char 5))))
+
+
+
+

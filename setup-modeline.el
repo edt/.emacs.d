79fee40899ae68c43c43439508e7dff776999fcd
Edgar Thier <edgarthier@gmail.com>
2013-07-21 15:29:01 +0200

    Introduced diminish mode

--- setup-modeline.el
+++ setup-modeline.el
@@ -1,4 +1,15 @@
 
+;; diminish minor modes to save space
+;; to view all minor modes C-h m
+(when (require 'diminish nil 'noerror)
+  (eval-after-load "company"
+      '(diminish 'company-mode "Cmp"))
+  (eval-after-load "abbrev"
+    '(diminish 'abbrev-mode "Ab"))
+  (eval-after-load "yasnippet"
+    '(diminish 'yas-minor-mode "Y"))
+  (eval-after-load "Projectile"
+    '(diminish 'projectile-mode "P")))
 
 ;; Show date and time in 24h format in modeline
 (setq display-time-day-and-date t)

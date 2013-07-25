94ba0aff90392f7cd86fc40ca5595ac632c9fecc
Edgar Thier <edgarthier@gmail.com>
2013-07-25 17:25:02 +0200

    Initial commit python configuration
        Added section concerning flymake pylint checks to allow automatic
        code evaluation

--- programming.el
+++ programming.el
@@ -44,3 +44,17 @@
 
 ;; cperl-mode is preferred to perl-mode
 (defalias 'perl-mode 'cperl-mode)
+
+;; Python section
+
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "epylint" (list local-file))))
+
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))

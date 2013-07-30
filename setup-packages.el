03cbcf998ac1a862a8f15e20db60b925b81b8bd2
Edgar Thier <edgarthier@gmail.com>
2013-07-30 16:10:56 +0200

    Basic package setup
        Repository list
        elpy as only package

--- setup-packages.el
+++ setup-packages.el
@@ -35,4 +35,27 @@
 ;; Initialisiere alles und so...
 (el-get 'sync my-packages)
 
+
+
+; list the packages you want
+(setq package-list '(highlight-indentation elpy))
+
+; list the repositories containing them
+(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+
 (provide 'setup-packages)

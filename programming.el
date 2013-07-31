8ae01d7fca36c1a807c30dd857c88683fe974f02
Edgar Thier <edgarthier@gmail.com>
2013-07-31 17:21:16 +0200

    Defined indent level for namespace as 0

--- programming.el
+++ programming.el
@@ -16,6 +16,7 @@
 (defun my-c-mode-common-hook ()
   ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
   (c-set-offset 'substatement-open 0)
+  (c-set-offset 'innamespace 0)
   ;; other customizations can go here
 
   (setq c++-tab-always-indent t)

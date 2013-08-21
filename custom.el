ed7e969997d64eb54228b2070e2708fa268a196d
Edgar Thier <edgarthier@gmail.com>
2013-08-21 14:09:57 +0200

    Added c-basic-offset to custom
        Only gets evaluated  as default when in custom section

--- custom.el
+++ custom.el
@@ -8,6 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
  '(exec-path (quote ("/usr/local/bin" "/usr/bin" "/bin" "/usr/local/games" "/usr/games" "/usr/lib/emacs/24.2/x86_64-linux-gnu" "/home/edt/bin"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

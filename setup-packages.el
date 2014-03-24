525230c585adf32082d11d1d584695d18e159049
Edgar Thier <info@edgarthier.net>
2014-03-24 22:05:10 +0100

    Added marmalade to repository list

--- setup-packages.el
+++ setup-packages.el
@@ -35,6 +35,8 @@
              '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives
              '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (when (not package-archive-contents)
   (package-refresh-contents))

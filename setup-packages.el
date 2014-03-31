197d719630d1ba0e92fad9d90fcf241d4579e435
Edgar Thier <edgarthier@gmail.com>
2014-03-31 20:45:36 +0200

    Reintroduces el-get to allow automatic installation of irony-mode

--- setup-packages.el
+++ setup-packages.el
@@ -57,4 +57,25 @@
 								   (package-installed-p x)))
                   (mapcar 'car package-archive-contents))))
 
+;; el-get
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
+;; now either el-get is `require'd already, or have been `load'ed by the
+;; el-get installer.
+(setq el-get-sources
+      '(el-get				; el-get is self-hosting
+        irony-mode
+))
+
+;; install new packages and init already installed packages
+(el-get 'sync)
+
 (provide 'setup-packages)

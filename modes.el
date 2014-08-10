7ed32685d9ba3573cc2abef69245a451d0a71057
Edgar Thier <edgarthier@gmail.com>
2014-08-10 13:26:09 +0200

    highlight-symbol now uses use-package.

--- modes.el
+++ modes.el
@@ -63,11 +63,6 @@
 (require 'smartparens-config)
 (smartparens-global-mode t)
 
-(require 'highlight-symbol)
-(global-set-key (kbd "C-<f3>") 'highlight-symbol-at-point)
-(global-set-key (kbd "<f3>") 'highlight-symbol-next)
-(global-set-key (kbd "S-<f3>") 'highlight-symbol-prev)
-(global-set-key (kbd "M-<f3>") 'highlight-symbol-remove-all)
 
 
 
@@ -101,10 +96,15 @@
 (add-to-list 'auto-mode-alist '("bash_aliases\\'" . shell-script-mode))
 
 (use-package expand-region
+(use-package highlight-symbol
   :defer t
   :bind
   (("C-'" . er/expand-region)
    ("C-;" . er/contract-region)))
+  (( "C-<f3>" . highlight-symbol-at-point)
+   ( "<f3>" . highlight-symbol-next)
+   ( "S-<f3>" . highlight-symbol-prev)
+   ( "M-<f3>" . highlight-symbol-remove-all)))
 
 
 (use-package ggtags

233be698f3d5478d20ce8ea4b1229ade96e95784
Edgar Thier <info@edgarthier.net>
2014-12-07 20:40:05 +0100

    Added my-create-non-existent-directory
    Create directories for newly opened files if they do not exist.

--- lisp/file-functions.el
+++ lisp/file-functions.el
@@ -90,4 +90,15 @@ This command is similar to `find-file-at-point' but without prompting for confir
           (message "Deleted file %s" filename)
           (kill-buffer))))))
 
+;; taken from http://iqbalansari.github.io/blog/2014/12/07/automatically-create-parent-directories-on-visiting-a-new-file-in-emacs/
+
+(defun my-create-non-existent-directory ()
+  (let ((parent-directory (file-name-directory buffer-file-name)))
+    (when (and (not (file-exists-p parent-directory))
+               (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
+      (make-directory parent-directory t))))
+
+(add-to-list 'find-file-not-found-functions #'my-create-non-existent-directory)
+
+
 (provide 'file-functions)

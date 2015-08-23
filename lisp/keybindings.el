45945940febb441a74040ccc222da22a3e233f10
Edgar Thier <info@edgarthier.net>
2015-08-23 18:05:03 +0200

    Added keybindings to simplify window management
    C-1
    C-2
    C-3
    C-0

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -29,11 +29,16 @@
 (global-set-key (kbd "M-<up>")    'windmove-up)
 (global-set-key (kbd "M-<down>")  'windmove-down)
 
-
 ;; Make windmove work in org-mode:
 (add-hook 'org-shiftup-final-hook 'windmove-up)
 (add-hook 'org-shiftleft-final-hook 'windmove-left)
 (add-hook 'org-shiftdown-final-hook 'windmove-down)
 (add-hook 'org-shiftright-final-hook 'windmove-right)
+
+(global-set-key (kbd "C-1") 'delete-other-windows)
+(global-set-key (kbd "C-2") 'split-window-below)
+(global-set-key (kbd "C-3") 'split-window-right)
+(global-set-key (kbd "C-0") 'delete-window)
+
 
 (provide 'keybindings)

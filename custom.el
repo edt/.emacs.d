9f3ee64108da9d70983f240bc5f48a1cd935a663
Edgar Thier <edgar.thier@theimagingsource.com>
2015-11-02 15:10:01 +0100

    Update custom.el

--- custom.el
+++ custom.el
@@ -9,6 +9,24 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(c-basic-offset 4)
- '(custom-safe-themes (quote ("38f84e8cd10ba4078b50a46f807be579b8f2e5a350bf3649a50a792fa274f8c7" default)))
- '(exec-path (quote ("/usr/local/bin" "/usr/bin" "/bin" "/usr/local/games" "/usr/games" "/usr/lib/emacs/24.2/x86_64-linux-gnu" "~/bin")))
+ '(cmake-ide-rc-executable "/home/edt/.emacs.d/el-get/rtags/bin/rc")
+ '(cmake-ide-rdm-executable "/home/edt/.emacs.d/el-get/rtags/bin/rdm")
+ '(custom-safe-themes
+   (quote
+    ("38f84e8cd10ba4078b50a46f807be579b8f2e5a350bf3649a50a792fa274f8c7" default)))
+ '(exec-path
+   (quote
+    ("/usr/local/bin" "/usr/bin" "/bin" "/usr/local/games" "/usr/games" "/usr/lib/emacs/24.2/x86_64-linux-gnu" "~/bin")))
+ '(rtags-completions-enabled t)
+ '(rtags-path "/home/edt/.emacs.d/el-get/rtags/bin")
+ '(safe-local-variable-values
+   (quote
+    ((cmake-ide-dir . "/home/edt/work/aravis/")
+     (cmake-ide-dir . "/home/edt/work/tiscamera/build"))))
  '(w3m-default-display-inline-images t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

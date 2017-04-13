fff03a2c4b6cec5fdb7f80a9457cb9dfce73346a
Edgar Thier <edgar.thier@theimagingsource.com>
2017-04-13 05:58:17 +0200

    Add function copy-file-name-to-clipboard

--- lisp/file-functions.el
+++ lisp/file-functions.el
@@ -1,4 +1,15 @@
 
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+
 (defun show-file-name ()
   "Show the full path file name in the minibuffer and add it to the clipboard."
   (interactive)

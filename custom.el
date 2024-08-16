e07b63d464460766a20482918c900434510cf4c4
Edgar Thier <info@edgarthier.net>
2024-08-16 13:15:40 +0200

    Add custom.el

---
+++ custom.el
@@ -0,0 +1,30 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
+ '(custom-safe-themes
+   '("3ea6a13b8119d69238e9651c05092d4491816b4d8066481e84e36901b6542089" default))
+ '(lsp-clangd-binary-path "/usr/bin/clangd")
+ '(package-selected-packages
+   '(highlight-doxygen fussy editorconfig symbol-overlay company elpy powershell multiple-cursors orderless marginalia smart-mode-line diff-hl smartparens expand-region smartparens-config lsp-mode vertico all-the-icons-completion all-the-icons flycheck goto-line-preview magit projectile beacon ws-butler indent-guide which-key dockerfile-mode cmake-mode groovy-mode amx ido-completing-read+ highlight-symbol rainbow-mode fullframe tangotango-theme))
+ '(safe-local-variable-values
+   '((cmake-ide-build-dir . "/home/edt/work/tiscamera/build")
+     (cmake-ide-project-dir . "/home/edt/work/tiscamera"))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(diff-hl-change ((t (:background "royal blue" :foreground "royal blue"))))
+;;  '(diff-hl-delete ((t (:inherit diff-removed :background "red3" :foreground "red3"))))
+;;  '(diff-hl-insert ((t (:inherit diff-added :background "green4" :foreground "green4")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-hl-change ((t (:background "royal blue" :foreground "royal blue"))))
+ '(diff-hl-delete ((t (:inherit diff-removed :background "red3" :foreground "red3"))))
+ '(diff-hl-insert ((t (:inherit diff-added :background "green4" :foreground "green4")))))
